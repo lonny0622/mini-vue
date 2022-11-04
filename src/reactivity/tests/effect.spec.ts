21deafb222cde2510a29d0d52a4950f2b9f2b897
mulan <649754673@qq.com>
2022-11-04 15:33:37 +0800

    feat: 实现scheduler

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -14,7 +14,44 @@ describe("effect", () => {
     expect(nextAge).toBe(11);
 
     //update
-    user.age++; 
+    user.age++;
     expect(nextAge).toBe(12);
+  });
+  it("should return effect runner", () => {
+    // 1. effect(fn) -> function (runner) -> fn -> return
+    let foo = 10;
+    const runner = effect(() => {
+      foo++;
+      return "foo";
+    });
+    expect(foo).toBe(11);
+    const r = runner();
+    expect(foo).toBe(12);
+    expect(r).toBe("foo");
+  });
+  it("scheduler", () => {
+    // 1. 通过 effect 的第二个参数给定的一个 scheduler 的 fn
+    // 2. effect 第一次执行时还会执行 fn
+    // 3. 当响应式对象 set update 不会执行 fn 而是执行 scheduler
+    // 4. 如果说执行 runner 的时候，会再次执行 fn
+    let dummy;
+    let run: any;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+    const obj = reactive({ foo: 1 });
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      { scheduler }
+    );
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1);
+    obj.foo++;
+    expect(scheduler).toHaveBeenCalledTimes(1);
+    expect(dummy).toBe(1);
+    run();
+    expect(dummy).toBe(2);
   });
 });
