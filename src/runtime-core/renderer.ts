38d4fdec96b1749de6e237ff5387e16e549dfc50
mulan <649754673@qq.com>
2022-11-09 16:12:06 +0800

    feat: 实现注册事件功能

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -38,7 +38,14 @@ function mountElement(vnode: VNode, container: HTMLElement) {
     const { props } = vnode;
     for (const key in props) {
         const val = props[key];
-        el.setAttribute(key, val);
+        console.log(key);
+        const isOn = (key: string) => /^on[A-Z]/.test(key);
+        if (isOn(key)) {
+            const event = key.slice(2).toLowerCase();
+            el.addEventListener(event, val);
+        } else {
+            el.setAttribute(key, val);
+        }
     }
 
     container.append(el);
