3d4d932a747761770e661bff5d990126831420aa
mulan <649754673@qq.com>
2022-11-17 14:35:41 +0800

    feat:  增加reactive模块类型注解与注释

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,11 +1,20 @@
 import { isHasChange, isObject } from "../shared";
-import { isTracking, trackEffects, triggerEffects } from "./effect";
+import {
+    isTracking,
+    ReactiveEffect,
+    trackEffects,
+    triggerEffects,
+} from "./effect";
 import { reactive } from "./reactive";
 
 class RefImpl {
+    // 对象的值
     private _value: any;
+    // 用于保存原始值
     private _rawValue: any;
-    public dep: Set<any>;
+    // 储存effect
+    public dep: Set<ReactiveEffect>;
+    // 是否为ref对象
     public __v_isRef = true;
     constructor(value: any) {
         // 保存原始值，便于之后对比
@@ -16,11 +25,16 @@ class RefImpl {
 
         this.dep = new Set();
     }
-
+    /**
+     * 获取值时进行依赖追踪
+     */
     get value() {
         trackRefValue(this);
         return this._value;
     }
+    /**
+     * 设置值时触发Effect
+     */
     set value(newValue) {
         // 只有value的值改变后才触发trigger
         if (isHasChange(this._value, newValue)) {
@@ -32,28 +46,56 @@ class RefImpl {
         }
     }
 }
-
+/**
+ * 进行依赖追踪
+ * @param ref
+ */
 function trackRefValue(ref: RefImpl) {
+    // 避免重复/或者调用了stop后追踪
     if (isTracking()) trackEffects(ref.dep);
 }
 
+/**
+ * 将值转化为reactive对象
+ * @param value
+ * @returns
+ */
 function convert(value: any) {
     return isObject(value) ? reactive(value) : value;
 }
 
+/**
+ * 创建ref对象
+ * @param value
+ * @returns
+ */
 export function ref(value: any) {
     return new RefImpl(value);
 }
-
+/**
+ * 判断是否为ref对象
+ * @param ref
+ * @returns
+ */
 export function isRef(ref: any) {
     return !!ref.__v_isRef;
 }
 
+/**
+ * 将对象的值暴露出来
+ * @param ref
+ * @returns
+ */
 export function unRef(ref: any) {
     //判断是不是ref对象
     return isRef(ref) ? ref.value : ref;
 }
 
+/**
+ * ref代理对象，可以直接获取或者设置ref对象的值，而不是通过ref.value
+ * @param objectWithRefs
+ * @returns
+ */
 export function proxyRefs(objectWithRefs: any) {
     return new Proxy(objectWithRefs, {
         get(target, key) {
